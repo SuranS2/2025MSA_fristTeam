dd803d7385614d9741f206c0f66bba58b47ab1cc
unknown <blackteatime4@naver.com>
2025-08-30 10:28:31 +0900

    feat/로또 번호 추출 메소드 생성

--- lotto/Lotto.java
+++ lotto/Lotto.java
@@ -6,14 +6,22 @@ public class Lotto {
 	Lotto(){
 		randNums = new int[6];
 	}
-	
-	// 번호 출력
-	void printRandNum() {
-		System.out.printf("[ ");
-		for(int randNum : randNums) {
-			System.out.printf("%d ",randNum);
-		}
-		System.out.println("]");
-	}
+
+    // 번호 출력
+    void printRandNum() {
+        System.out.printf("[ ");
+        for(int randNum : randNums) {
+            System.out.printf("%d ",randNum);
+        }
+        System.out.println("]");
+    }
+
+
+    // 랜덤 번호 최대 45번 까지 추출
+    private void pickSixRandomNumbers() {
+        for (int i = 0; i < randNums.length; i++) {
+            randNums[i] = (int) (Math.random() * 45 + 1);
+        }
+    }
 
 }
