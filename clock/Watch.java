abbb9f573a60c4a193c993ebeb7ef84462bcc2b9
NaGihyeon <nesd0957@gmail.com>
2025-08-30 16:04:51 +0900

    update class Watch

--- clock/Watch.java
+++ clock/Watch.java
@@ -1,7 +1,40 @@
 package clock;
 
-public class Watch {
-	
-	Display display;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
+public class Watch {
+	private String watchSerialNum;	//시계 시리얼 넘버
+	private int watchSize;	//시계 직경
+	private String ownerName;	//소유자 이름
+	
+	//부품
+	private Display display;
+	
+	public Watch() {
+		this("소유자");
+	}
+	
+	public Watch(String ownerName) {
+		this.watchSerialNum = createWatchSerialNum();
+		this.watchSize = 28;
+		this.ownerName = ownerName;
+	}
+	
+	private String createWatchSerialNum(){
+		return UUID.randomUUID().toString().replace("-", "").substring(0,11);
+	}
+	
+	
+	//기능
+	//시간 확인 기능
+	public void printTime() {
+		System.out.println(LocalDateTime.now());
+	}
+	
+	//날짜 확인 기능
+	public void printDate() {		
+		System.out.println(LocalDate.now());
+	}
 }
