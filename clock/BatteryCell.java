242bb4ffd1d206002e446fbf4b886c4ee9c7e65f
Sur0n <skyzephyr333@gmail.com>
2025-08-30 16:15:10 +0900

    add Battery, BatteryCell

---
+++ clock/BatteryCell.java
@@ -0,0 +1,11 @@
+package clock;
+
+public class BatteryCell {
+	private String SerialNumber;
+	public BatteryCell() {
+		SerialNumber = initSerial();
+	}
+	private String initSerial() {
+		return null;
+	}
+}
--- clock/Watch.java
+++ clock/Watch.java
@@ -1,5 +1,5 @@
 package clock;
 
 public class Watch {
-
+	Display
 }
