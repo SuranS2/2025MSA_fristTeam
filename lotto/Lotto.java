f5a4dab907cd8809a0d1824c0a8e8d57fd32dc18
gmkoo-d3v <gunmohkoo@naver.com>
2025-08-30 11:23:14 +0900

    validDupNum name update

--- lotto/Lotto.java
+++ lotto/Lotto.java
@@ -38,16 +38,18 @@ public class Lotto {
 		return arr;
 	}
 	
-	// 번호 확인
-	void valiNum(int[] randNums){
+		// 번호 확인
+	boolean validDupNum(){
+		
+		//배열로 새로들어올 숫자를 기존배열에서 중복되는지 확인후 중복되면 true 종복안되면 false 로 반환
 		for(int i=0; i<randNums.length;i++){
-			for(int j=0; j<i; j++){
-				if(randNums[i] == randNums[j]){
-					i--;
-					break;
+			for(int j=0; j<randNums.length; j++){
+				if(randNums[i] == randNums[j]){ 
+					return true;
 				}
 			}
-		}
+		} 
+		return false;
 	}
 
 }
