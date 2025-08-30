2a7b3635ab23b4da9bad09e28d9456fde725eb35
NaGihyeon <nesd0957@gmail.com>
2025-08-30 11:30:30 +0900

    update bubbleSort

--- lotto/Lotto.java
+++ lotto/Lotto.java
@@ -17,7 +17,7 @@ public class Lotto {
 		System.out.println("]");
 	}
 	
-	public int[] bubbleSort(int[] arr) {
+	public void bubbleSort(int[] arr) {
 		for(int i=arr.length-1; i>0; i--) {
 			for(int j=0; j<i; j++) {
 				if(arr[j]>arr[j+1]) {
@@ -27,7 +27,6 @@ public class Lotto {
 				}
 			}
 		}
-		return arr;
 	}
 	
 	// 번호 확인
